d5e3335e14de37ee42dd80f927fa8326dded45be
DariaGavrilenko <gavrilenko10919@gmail.com>
2023-02-06 13:58:33 +0300

    fix

--- src/components/Header/components/Nav/Nav.tsx
+++ src/components/Header/components/Nav/Nav.tsx
@@ -5,8 +5,6 @@ import style from './Nav.module.css';
 const Nav = () => {
   return (
     <div className={style.NavContainer}>
-      {/* <span>Main</span>
-      <span>Skills</span> */}
       <a href="#skill" className={style.link}>Skill</a>
       <a href="#projects" className={style.link}>Projects</a>
       <a href="#contacts" className={style.link}>Contacts</a>
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -3,7 +3,6 @@ import style from './Loading.module.css'
 const Loading = () =>{
     return (
         <div className={style.loading}>
-            {/* <img src='https://sezeromer.com/wp-content/uploads/2019/09/Infinity-1s-200px.gif' alt="" /> */}
             <img src='https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif' alt="" />
         </div>
     ) 
