7faf7a1763cce32965f045a6d035f8bcc3684293
DariaGavrilenko <gavrilenko10919@gmail.com>
2023-04-20 14:14:27 +0300

    feat: add notes project

--- src/components/MyProjects/MyProjects.constants.ts
+++ src/components/MyProjects/MyProjects.constants.ts
@@ -21,5 +21,11 @@ export const myProjects = {
         backgroundImage: landingImg,
         link: 'https://dariagavrilenko.github.io/landings/',
         description: `In my free time, I try to repeat the landing page from figma.`
+    }, 
+    notes:{
+        projectName: 'Notes',
+        backgroundImage: `https://www.wikihow.com/images/thumb/1/18/Take-Better-Notes-Step-1-Version-2.jpg/v4-460px-Take-Better-Notes-Step-1-Version-2.jpg.webp`,
+        link: 'https://neon-travesseiro-b73146.netlify.app/',
+        description: `You can create, delete and edit note. You can also create keywords with # and search for them. Notes be kept in local storage`
     }
 }
--- src/components/MyProjects/MyProjects.tsx
+++ src/components/MyProjects/MyProjects.tsx
@@ -16,6 +16,7 @@ const MyProjects = () => {
       <div className={`${styleContainer.container} ${style.container}`}>
         <Project projectName={myProjects.social.projectName} discriptions={myProjects.social.description} img={myProjects.social.backgroundImage} link={myProjects.social.link} />
         <Project projectName={myProjects.toDo.projectName} discriptions={myProjects.toDo.description} img={myProjects.toDo.backgroundImage} link={myProjects.toDo.link} />
+        <Project projectName={myProjects.notes.projectName} discriptions={myProjects.notes.description} img={myProjects.notes.backgroundImage} link={myProjects.notes.link} />
         <Project projectName={myProjects.landings.projectName} discriptions={myProjects.landings.description} img={myProjects.landings.backgroundImage} link={myProjects.landings.link} />
       </div>
     </div>
